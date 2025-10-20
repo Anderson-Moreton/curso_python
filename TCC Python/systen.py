3ec91cf7dd44c7447b3fbe538ec3676f1d95ea04
Anderson Moreton <anderson.moreton@gmail.com>
2025-10-20 23:46:56 +0100

    TCC with Python

---
+++ TCC Python/systen.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class Gui():
+    """Classe para criar a interface gráfica do sistema.    
+    """
+    x_pad = 5
+    y_pad = 3
+    width_entry = 30
+
+#Para criar uma janela
+#PYSQL será o nome fantasia da janela
+window = Tk()
+window.wm_title("PYSQL version 1.0")
+
